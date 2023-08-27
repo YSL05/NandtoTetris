bbb536b5cf68dd788a7eaeba5044e587eb92a66f
slyyy@123.com <slyyy@123.com>
2023-08-27 18:46:49 +0800

    通过机器语言实现两个数相乘及键盘输入和屏幕输出

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -1,14 +1,46 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+@SCREEN
+D = A
+@cur
+M = D  //初始化光标的位置
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+//判断是否存在输入，无输入跳转到loop2,清0，有输入跳转到loop1,置1
+(loop)
+@KBD
+D = M
+@loop2
+D;JEQ   
 
-// Put your code here.
+//先判断光标是否还有空余，有空余填充1
+@cur
+D = M
+@KBD
+D = D - A
+@end
+D;JEQ
+@cur
+A = M
+M = 1
+@cur
+M = M + 1
+@loop
+0;JMP
+
+//先判断光标是否到屏幕的起点，没有到填充0
+(loop2)
+@cur
+D = M
+@SCREEN
+D = D - A
+@end
+D;JEQ
+@cur
+M = M - 1
+A = M
+M = 0
+@loop
+0;JMP
+
+//回到开始继续判断是否键盘有输入
+(end)
+@loop
+0;JMP
